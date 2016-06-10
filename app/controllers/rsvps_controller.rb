2ca5e93ace3f881bf15a97ae48bf14ac040ac104
Mark Richardson <markeban@yahoo.com>
2016-06-09 20:02:02 -0700

    DRYed code usinga partial

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -5,25 +5,40 @@ class RsvpsController < ApplicationController
   end
 
   def create
-    rsvp = Rsvp.new(rsvp_params)
-    rsvp.save
+    @rsvp = Rsvp.new(rsvp_params)
+    if @rsvp.save
+      redirect_to "/rsvps/#{@rsvp.id}"
+    else
+      render :new
+    end
   end
-
 
   def show
-    
+    @rsvp = Rsvp.find_by(id: params[:id])
   end
 
+  def edit
+    @rsvp = Rsvp.find_by(id: params[:id])
+  end
 
+  def update
+    @rsvp = Rsvp.find_by(id: params[:id])
+    if @rsvp.update(rsvp_params)
+      redirect_to "/rsvps/#{@rsvp.id}"
+    else
+      render :edit
+    end
+  end
 
-
-
-
+  def destroy
+    Rsvp.find_by(id: params[:id]).destroy
+    redirect_to "/rsvps"
+  end
 
   private
 
   def rsvp_params
-    params.require(:rsvp).permit(:first_name, :last_name, :attending)
+    params.require(:rsvp).permit(:first_name, :last_name, :attending, :vegetarian, :guests  )
   end
 
 
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,2 +1,3 @@
 class Rsvp < ActiveRecord::Base
+  validates :first_name, presence: true
 end
